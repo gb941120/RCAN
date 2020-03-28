83915a866ab3484d7d7707b3679ca7ae6b96b5a2
Gong Bo <47877232+gb941120@users.noreply.github.com>
2020-03-28 18:14:18 +0800

    Update common.py

--- RCAN_TrainCode/code/model/common.py
+++ RCAN_TrainCode/code/model/common.py
@@ -15,16 +15,16 @@ class MeanShift(nn.Conv2d):#继承nn.Conv2d
     def __init__(self, rgb_range, rgb_mean, rgb_std, sign=-1):#定义初始化函数参数包括，自变量和实例参数。
         super(MeanShift, self).__init__(3, 3, kernel_size=1)#自定义类初始化.使用初始话函数__init__
         std = torch.Tensor(rgb_std)#将颜色通道的方差转换成张量。
-        self.weight.data = torch.eye(3).view(3, 3, 1, 1)
+        self.weight.data = torch.eye(3).view(3, 3, 1, 1)#形状为（3，3，1，1）的张量
         self.weight.data.div_(std.view(3, 1, 1, 1))
-        self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean)
+        self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean)#偏置为
         self.bias.data.div_(std)
         self.requires_grad = False
-
+#定义基础模块，继承顺序类
 class BasicBlock(nn.Sequential):
     def __init__(
         self, in_channels, out_channels, kernel_size, stride=1, bias=False,
-        bn=True, act=nn.ReLU(True)):
+        bn=True, act=nn.ReLU(True)):#定义初始化函数
 
         m = [nn.Conv2d(
             in_channels, out_channels, kernel_size,
@@ -32,45 +32,53 @@ class BasicBlock(nn.Sequential):
         ]
         if bn: m.append(nn.BatchNorm2d(out_channels))
         if act is not None: m.append(act)
-        super(BasicBlock, self).__init__(*m)
-
+        super(BasicBlock, self).__init__(*m)#初始化类。
+#定义残差块，继承nn.Module
 class ResBlock(nn.Module):
+    #定义初始化函数
     def __init__(
         self, conv, n_feat, kernel_size,
         bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
 
-        super(ResBlock, self).__init__()
+        super(ResBlock, self).__init__()#初始化类
         m = []
         for i in range(2):
             m.append(conv(n_feat, n_feat, kernel_size, bias=bias))
-            if bn: m.append(nn.BatchNorm2d(n_feat))
-            if i == 0: m.append(act)
+            if bn: 
+                m.append(nn.BatchNorm2d(n_feat))
+            if i == 0: 
+                m.append(act)
 
         self.body = nn.Sequential(*m)
         self.res_scale = res_scale
-
+#定义反馈网络。
     def forward(self, x):
         res = self.body(x).mul(self.res_scale)
         res += x
 
         return res
-
+#上采样
 class Upsampler(nn.Sequential):
+    #定义初始化函数
     def __init__(self, conv, scale, n_feat, bn=False, act=False, bias=True):
 
         m = []
         if (scale & (scale - 1)) == 0:    # Is scale = 2^n?
-            for _ in range(int(math.log(scale, 2))):
+            for _ in range(int(math.log(scale, 2))):#_表示一个临时变量，无关紧要的。
                 m.append(conv(n_feat, 4 * n_feat, 3, bias))
                 m.append(nn.PixelShuffle(2))
-                if bn: m.append(nn.BatchNorm2d(n_feat))
-                if act: m.append(act())
+                if bn: 
+                    m.append(nn.BatchNorm2d(n_feat))#向m中加入元素
+                if act: 
+                    m.append(act())
         elif scale == 3:
             m.append(conv(n_feat, 9 * n_feat, 3, bias))
             m.append(nn.PixelShuffle(3))
-            if bn: m.append(nn.BatchNorm2d(n_feat))
-            if act: m.append(act())
+            if bn: 
+                m.append(nn.BatchNorm2d(n_feat))
+            if act: 
+                m.append(act())
         else:
             raise NotImplementedError
 
-        super(Upsampler, self).__init__(*m)
+        super(Upsampler, self).__init__(*m)#初始化类
